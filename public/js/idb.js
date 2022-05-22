62ae1fec15381b4764bf2d6347257eb5f0b694b8
Erica <erica.trenholm48@gmail.com>
2022-05-22 14:39:07 -0400

    upload data after reconnection

--- public/js/idb.js
+++ public/js/idb.js
@@ -8,20 +8,20 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function(event) {
     const db = event.target.result
     db.createObjectStore('new_transaction', { autoIncrement: true })
-};
+}
 
 // if successful
 request.onsuccess = function(event) {
     db = event.target.result
     if (navigator.onLine) {
-        // upload data
+        uploadRecord()
     }
-};
+}
 
 // if error
 request.onerror = function(event) {
     console.log(event.target.errorCode);
-};
+}
 
 // if no internet connection
 function saveRecord(record) {
@@ -29,3 +29,39 @@ function saveRecord(record) {
     const dataObjectStore = transaction.objectStore('new_transaction')
     dataObjectStore.add(record)
 }
+
+// when internet connection is reestablished
+function uploadRecord() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite')
+    const dataObjectStore = transaction.objectStore('new_transaction')
+    const getAll = dataObjectStore.getAll()
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch(`/api/transaction`, {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse)
+                }
+                const transaction = db.transaction(['new_transaction'], 'readwrite')
+                const dataObjectStore = transaction.objectStore('new_transaction')
+                dataObjectStore.clear()
+
+                alert('All saved transactions have been submitted.')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
+}
+
+window.addEventListener('online', uploadRecord)
